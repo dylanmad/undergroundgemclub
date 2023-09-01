5d9a84317d48e10dab1ed27382412cfddbce583d
Dylan Madhani <dmadbusiness@gmail.com>
2023-09-01 18:34:09 -0400

    Organized Javascript into seperate file JS that was in index.html in script tags is now in main.js.

---
+++ main.js
@@ -0,0 +1,180 @@
+
+function changeImage(a) {
+    document.getElementById("yt").src=a;
+}
+
+document.getElementById("myLink").onclick = function() {
+	/* Setting each album to Youtube link */
+	var link = document.getElementById("hw-mfdoom");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLgQVxrX6-iaIfnbbOzDH7J6t-MGwr1ucQ");
+	var link = document.getElementById("dday-dreamville");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLYKR7s8GyMtTGWsvgow3X_Rp3vDRHpZio");
+	var link = document.getElementById("tmnr-mfdoom");
+	link.setAttribute("href", "https://youtu.be/naLt3thjfi4");
+	var link = document.getElementById("10d-chancetherapper");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=MBCqnQB5GWY&list=PLDKGyuIV4Jy3VVN2B4oWqe-iSV4FjgLhp");
+	var link = document.getElementById("hw-jdilla");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLgQVxrX6-iaK1YRGC1b8MSfO86WoWk8wo");
+	var link = document.getElementById("ar-chancetherapper");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=g59OHgF--2Y&list=PL9y_aRAKmsdtjl0bbaAaljXLjQoBi9hKC");
+	var link = document.getElementById("e-frankocean");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=KE5_fkpEkNk&list=PLDkctdxuikDpSH9xCzMFChZm2zySW1YN1");
+	var link = document.getElementById("pd-docd");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=pxENAF52rjs&list=PLhgxy3IancXmUXpyNiv6A-0M4NNC3FwU9");
+	var link = document.getElementById("e-earlsweatshirt");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=dCqCED_0ZuU&list=PLyJ_kDGjhn0HqHcyR-9lwYYjTbTZglzTP");
+	var link = document.getElementById("b-tylerthecreator");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=I98_o7LFvC0&list=PL8YH4mOwWryXQlsxaaRkJsIGTbjCK4wlX");
+	var link = document.getElementById("rf-rickross");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=KYkQK7BoLsA&list=PL1DF243317BC8FACE");
+	var link = document.getElementById("dbr-travisscott");
+	link.setAttribute("href", "https://youtu.be/naLt3thjfi4");
+	var link = document.getElementById("aknc-kidcudi");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=WYJ3s9dXq98&list=PL1EBAE8ACCD7642B9");
+	var link = document.getElementById("fnl-jcole");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=gFC4jsNAAOE&list=PL9y_aRAKmsdvKCfS3XEx9n5OOkZ58cdr4");
+	var link = document.getElementById("1999-joeybadass");
+	link.setAttribute("href", "https://www.youtube.com/watch?v=vWUPOfKUhNQ&list=PLn7qZ70jqTZE6_sQPPefkcZaiKY_tt4YC");
+	var link = document.getElementById("delic-mac");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL2E96EC21A48B1063");
+	var link = document.getElementById("house-week");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_lj1KcaPJYjA1n29DoIF68QMbDehOplyEI");
+	var link = document.getElementById("dream2-meek");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLF09A750BC4AC0418");
+	var link = document.getElementById("love-asap");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL9y_aRAKmsdunqbLW5METOdSzmvdMXeWS");
+	var link = document.getElementById("overly-kdot");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL9y_aRAKmsdvyExR3YQn23BZzMWOTtJ6P");
+	var link = document.getElementById("royalty-gambino");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLMS9d-aS4gcxku4cGFFOWH6pWND3PJT11");
+	var link = document.getElementById("no1-wayne");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL27C08C4B05BBEAAA");
+	var link = document.getElementById("ultra-frank");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLx0q7abKXxa9jm1cYiIcuWVmEgbCXpx3e");
+	var link = document.getElementById("nothing-wale");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL9y_aRAKmsduYe1Hyf-ZErhVCM4Z1EC4X");
+	var link = document.getElementById("mad-villain");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL9dk_xtWpAkKs1-EKcvq-nKwdaaS-3czd");
+	var link = document.getElementById("labordays-rock");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL5jqJ2fm-QngnzEuECaonv4Hh6SyIYRmp");
+	var link = document.getElementById("3030-del");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLcqxxF8AYd2yJNJ3lSUVZ-Zm9AII_ZKMK");
+	var link = document.getElementById("listen-littlebro");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLkwFMG_Ch8ocG8I3UFqMywyR4XaQI3fvS");
+	var link = document.getElementById("damage-elp");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLEUtmr9t9qQQM5j2-797HcJjTIn2E1AZt");
+	var link = document.getElementById("ugly-atmos");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL107752F4937CBC1E");
+	var link = document.getElementById("violent-jedi");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLHM1MRwUfb-ghdvSbd1JMuEcEvWDPyHiW");
+	var link = document.getElementById("third-hiero");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_nNJ0TuexrUtaqxI9PURgmJ16Bay7VTAX4");
+	var link = document.getElementById("operation-mf");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_nENZLyEbKKR6TbLUd0QCZ6DHoREZ3N50o");
+	var link = document.getElementById("below-blu");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL486254D04B96B85C");
+	var link = document.getElementById("over-atmo");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL60072C0AFBBEA9CB");
+	var link = document.getElementById("piece-cunnin");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLHBy6KvQFrxRH2ArdSTuRIOpT4vI1RsTt");
+	var link = document.getElementById("water-leak");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLn7qZ70jqTZHbbX4WFUn8i4-KoN4DAQan");
+	var link = document.getElementById("hope-nonpro");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_nkXEO7rlY3kMBN4b7Mi3AYeBMR5CHUKKU");
+	var link = document.getElementById("champ-ali");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_nsb3zk0QFC5YqXeL-OH5HwQmfHJxEhw6g");
+	var link = document.getElementById("8mil-soul");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL5E4D352C1D628FD8");
+	var link = document.getElementById("bftd-keef");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL1032BC1347630CF2");
+	var link = document.getElementById("bftd2-keef");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLwVPbrOTqTSCBGzA5_PpOQSi0na2yXC7a");
+	var link = document.getElementById("bftd3-keef");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLPRWtKgY2MOuntX7zuenRB9mfbb1xlotk");
+	var link = document.getElementById("llty-mac");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_mFkO34clkwqFIS_kB8MSmA1CvN9gy_aao");
+	var link = document.getElementById("ros1-mac");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLN6gWrVPLNTM4ck_bb4xVCs66HnO9kSG1");
+	var link = document.getElementById("ros2-mac");
+	link.setAttribute("href", "https://youtu.be/ADtcu0R5BoA");
+	var link = document.getElementById("bm-future");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PL9tY0BWXOZFtUwTDcCsugv5qXfRUaO-70");
+	var link = document.getElementById("dd3-lilwayne");
+	link.setAttribute("href", "https://youtube.com/playlist?list=PLX6AiyuwRGY4H72F7gYQJyRJc58ZX_VJH");
+	var link = document.getElementById("yrn-migos");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_ndqi0SHHCUXvVkwn--iYqLG_NaQPbuAx0");
+	var link = document.getElementById("rev-xxx");
+	link.setAttribute("href", "https://youtube.com/playlist?list=OLAK5uy_m7l8pAn5tynGPouW3DnxXIYekotptdrOA");
+	var link = document.getElementById("97-ye");
+	link.setAttribute("href", "https://youtu.be/xZrXvUzQfBg");
+
+	return false;
+}
+
+
+
+function filterSelection(c) {
+	var x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c == "all") c = "";
+	for (i = 0; i < x.length; i++) {
+		w3RemoveClass(x[i], "show");
+	if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+	 }
+}
+
+function w3AddClass(element, name) {
+ var i, arr1, arr2;
+ arr1 = element.className.split(" ");
+ arr2 = name.split(" ");
+ for (i = 0; i < arr2.length; i++) {
+   if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+}
+}
+
+function w3RemoveClass(element, name) {
+ var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+ while (arr1.indexOf(arr2[i]) > -1) {
+ arr1.splice(arr1.indexOf(arr2[i]), 1);     
+ }
+}
+ element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+btns[i].addEventListener("click", function(){
+ var current = document.getElementsByClassName("active");
+current[0].className = current[0].className.replace(" active", "");
+ this.className += " active";
+});
+}
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	  mybutton.style.display = "block";
+	 } else {
+	 mybutton.style.display = "none";
+	 }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	 document.body.scrollTop = 0;
+	 document.documentElement.scrollTop = 0;
+}
+
+
+
+
