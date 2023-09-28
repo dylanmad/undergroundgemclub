b04575ac6a0dc55f147edc8c9db8f28ab43cf76b
Dylan Madhani <dmadbusiness@gmail.com>
2023-09-27 23:14:45 -0400

    Cleaned up spacing; Connected test.js to data.json Site is now static and ready for Github Pages.

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://localhost:5000';
-    fetch(`${baseUrl}/api/data`)
+//const baseUrl = 'http://localhost:5000';
+    fetch(`/data.json`)
       .then((response) => response.json())
       .then((data) => {
         // Process and populate the table with the data
